eadf219f3129e49edc2ac5b09d4e2e0e1315d0e9
Chris Nelson <ccelson@Chriss-MacBook-Pro.local>
2024-07-31 14:24:03 -0500

    Scripting of projections, combinations and json conversions

--- ClimateData/modules/orchestrator.py
+++ ClimateData/modules/orchestrator.py
@@ -1,11 +1,48 @@
 import projection_api as api
 import pandas as pd
 
-raw_projection_df = api.retrieveData("New York", "2024-01-01", "2025-01-01")
+# Get Input from User
+city_name = input("What is the City?")
+data_value = 'tempmax'
 
-max_temp_df = raw_projection_df[['datetime','data']]
+# Retrieve the projected data
+raw_projection_df = api.retrieveData(city_name, "2024-08-01", "2050-06-01")
+# Process projected data
+projected_max_temp_df = raw_projection_df[['datetime','data']]
+projected_max_temp_df['datetime'] = pd.to_datetime(projected_max_temp_df['datetime'])
+projected_max_temp_df = projected_max_temp_df.groupby([pd.Grouper(key='datetime', freq='1M')]).max()
 
-max_temp_df['datetime'] = pd.to_datetime(max_temp_df['datetime'])
-max_temp_df = max_temp_df.groupby([pd.Grouper(key='datetime', freq='1M')]).max()
-# print(df.tail(20))
-print(max_temp_df.tail(20))
+# Rename column to align with historical data
+projected_max_temp_df.rename(columns={'data': data_value}, inplace=True)
+
+# Save the processed data
+projected_max_temp_df.to_csv('../'+city_name+'/Processed/projected_'+data_value+'_monthly.csv')
+projected_max_temp_df.to_json('../'+city_name+'/Processed/projected_'+data_value+'_monthly.json', orient='table')
+
+# Retrieve the historical data
+input_file = '../'+city_name+'/Raw/historical_daily_raw.csv'
+raw_historical_df = pd.read_csv(input_file)
+
+# Process historical data
+historical_max_temp_df = raw_historical_df[['datetime',data_value]]
+historical_max_temp_df['datetime'] = pd.to_datetime(historical_max_temp_df['datetime'])
+historical_max_temp_df = historical_max_temp_df.groupby([pd.Grouper(key='datetime', freq='1M')]).max()
+historical_max_temp_df[data_value] = historical_max_temp_df[data_value].map(lambda a: (a-32) * 5/9)
+
+# Save the processed data
+output_location = '../'+city_name+'/Processed/historical_'+data_value+'_monthly'
+historical_max_temp_df.to_csv(output_location+'.csv')
+historical_max_temp_df.to_json(output_location+'.json', orient='table')
+
+# Combine the historical & projected data
+combined_df = pd.concat([historical_max_temp_df, projected_max_temp_df])
+
+# calculate historical average over last two years
+output_location = '../'+city_name+'/Processed/combined_'+data_value
+combined_df.to_csv(output_location+'.csv')
+combined_df.to_json(output_location+'.json', orient='table')
+# determine if projected temp needs offset
+
+# apply offset to projected data
+
+# Combine the historical & projected data
